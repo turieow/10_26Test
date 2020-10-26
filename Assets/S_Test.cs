34eb1d64ae5d59f4a001540e1aae4a3b88ee41a3
ndiolbydx <mai_19n1102996@nnn.ed.jp>
2020-10-26 15:05:11 +0900

    Comito

---
+++ Assets/S_Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var controllerNames = Input.GetJoystickNames();
+
+        print(controllerNames);
+        //// 一台もコントローラが接続されていなければエラー
+        if (controllerNames[0] == "") Debug.Log("Error");
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        if(Input.anyKey)
+        {
+            print("A");
+        }
+    }
+}
